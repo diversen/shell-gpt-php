04e084cb4567f69564e3a24b074dca3c9d1aa761
Dennis Iversen <dennis.iversen@gmail.com>
2023-02-20 19:33:26 +0100

    create dirs if dirs do not exist

--- src/Base.php
+++ src/Base.php
@@ -21,15 +21,18 @@ class Base
         $this->utils = new Utils();
     }
 
-    private function getKeyFile() {
-        $home = getenv("HOME");
-        $file = $home . '/.config/shell-gpt/api_key.txt';
+    private function getKeyFile()
+    {
+        $key_dir = getenv("HOME") . '/.config/shell-gpt';
+        if (!file_exists($key_dir)) {
+            mkdir($key_dir, 0700, true);
+        }
+        $file = $key_dir . '/api_key.txt';
         return $file;
     }
 
     private function getApiKey()
     {
-
         $file = $this->getKeyFile();
         if (file_exists($file)) {
             return trim(file_get_contents($file));
@@ -42,7 +45,6 @@ class Base
 
     public function getBaseParams(\Diversen\ParseArgv $parse_argv)
     {
-
         $models = [
             'davinci' => 'text-davinci-003',
             'curie' => 'text-curie-001',
@@ -66,7 +68,6 @@ class Base
     public function openAiRequest($params)
     {
 
-        // Send request to OpenAI API
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, 'https://api.openai.com/v1/completions');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -82,7 +83,8 @@ class Base
 
         $result = curl_exec($ch);
         if (curl_errno($ch)) {
-            echo 'Error:' . curl_error($ch);
+            print('Request error:' . curl_error($ch));
+            exit(1);
         }
         curl_close($ch);
 
@@ -97,7 +99,7 @@ class Base
 
         if ($error) {
             print($result["error"]["message"] . PHP_EOL);
-            print("You may also check existing key file: ". $this->getKeyFile() . PHP_EOL);
+            print("You may also check existing key file: " . $this->getKeyFile() . PHP_EOL);
             exit(1);
         }
         $text = trim($result["choices"][0]["text"]);
